c7e6eda499aaa0bbf8f1ab387bf3dcd40b8fa7c0
OUKACHBI_Abderrahmane <oukachbiabderrahmane0@gmail.com>
2026-04-21 11:29:35 +0200

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,43 +1,3 @@
-"""
-import tkinter as tk
-
-root = tk.Tk()
-image = tk.PhotoImage(file="rain.gif")
-
-def turn_tv_on():
-    window = tk.Toplevel(root)
-    window.title("TV")
-    original_image = tk.Label(window, image=image)
-    original_image.pack()
-
-def volume_up():
-    print("Volume Increase +1")
-
-def volume_down():
-    print("Volume Decrease -1")
-
-turn_on = tk.Button(root, text="ON", command=turn_tv_on)
-turn_on.pack()
-
-turn_off = tk.Button(root, text="OFF", command=root.destroy)
-turn_off.pack()
-
-volume = tk.Label(root, text="VOLUME")
-volume.pack()
-
-vol_up = tk.Button(root, text="+", command=volume_up)
-vol_up.pack()
-
-vol_down = tk.Button(root, text="-", command=volume_down)
-vol_down.pack()
-
-
-root.mainloop()
-"""
-
-
-
-
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
@@ -45,55 +5,13 @@ import mediapipe as mp
 import numpy as np
 import keras
 import cv2
+from time import time
 
 root = Tk()
 root.geometry("550x300+300+150")
 root.resizable(width=True, height=True)
 
-# Returns a file name that is chosen by the user
-def openfn():
-    return filedialog.askopenfilename(title='open')
-
-# Action being made when clicking the button 'open image'
-def open_img():
-    global global_img
-    # We select the filename
-    filename = openfn()
-    if filename:
-        # We open the image and convert it to RGB
-        img = Image.open(filename).convert("RGB")
-        # We store its numpy array into a global variable
-        global_img = np.array(img)
-        # We resize the image
-        w, h = img.size
-        new_w = 500
-        new_h = int(h * (new_w/w))
-        img = img.resize((new_w, new_h), Image.LANCZOS)
-        # We convert it to an ImageTk and change the panel image
-        img = ImageTk.PhotoImage(img)
-        panel.configure(image=img)
-        panel.image = img
-
-def take_screenshot():
-    global global_img
-    # We take a caption, flip it and convert it to RGB
-    capture = cv2.VideoCapture(0)
-    _, img = capture.read()
-    img = cv2.flip(img, 1)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # We store its numpy array into a global variable
-    global_img = img
-    # We convert the caption into an Image
-    img = Image.fromarray(img)
-    # We resize the image
-    w, h = img.size
-    new_w = 500
-    new_h = int(h * (new_w/w))
-    img = img.resize((new_w, new_h), Image.LANCZOS)
-    # We convert it to an ImageTk and change the panel image
-    img = ImageTk.PhotoImage(img)
-    panel.configure(image=img)
-    panel.image = img
+"""Functions that are not about Tkinter"""
 
 def bound(x, mini, maxi):
     return min(max(x, mini), maxi)
@@ -137,9 +55,9 @@ def prediction_to_symbol(prediction):
     symbol.append(if_float(prediction[14], 2, 'does not touch index finger', 'touches index finger'))
     return symbol
 
-def use_mnist():
-    img = np.copy(global_img)
-    res = detector.detect(mp.Image(image_format=mp.ImageFormat.SRGB, data=global_img))
+# Takes an image (a 3D RGB array) and returns an OpenCV MNIST image centered on the hand (and None if there is no hand)
+def img_to_mnist(img):
+    res = detector.detect(mp.Image(image_format=mp.ImageFormat.SRGB, data=img))
     h, w, _ = img.shape
     hands = np.array([[[point.x*w, point.y*h, point.z*np.mean([w, h])] for point in hand] for hand in res.hand_landmarks])
     if len(hands):
@@ -156,16 +74,75 @@ def use_mnist():
         # We crop the image
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)[(center_y - side//2):(center_y + side//2), (center_x - side//2):(center_x + side//2)]
         # We reduce the image to a 28 * 28 pixel one
-        mnist_img = np.reshape(cv2.resize(img, (28, 28)), (1, 28, 28, 1))/255
+        return np.reshape(cv2.resize(img, (28, 28)), (1, 28, 28, 1))/255
+    
+
+"""Tkinter functions"""
+
+# Returns a file name that is chosen by the user
+def openfn():
+    return filedialog.askopenfilename(title='open')
+
+# Prints the result of the model on the given image (as an array)
+def use_mnist(img):
+    # We convert the image to MNIST
+    mnist_img = img_to_mnist(img)
+    # If a hand was detected
+    if mnist_img is not None:
         # We apply our model to the image
         output = model(mnist_img)[0]
-        output = np.round(output, 3)
         # We convert our prediction to symbols
-        print(prediction_to_symbol(output))
+        print(prediction_to_symbol(np.round(output, 3)))
 
+def recording_loop():
+    global global_img
+    # If the camera is open
+    if capture:
+        """About recording the video"""
+        # We take a caption and save it
+        _, img = capture.read()
+        out.write(img)
+        # We convert it to RGB and keep an array version of it
+        img_array = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # We convert it into an Image
+        img = Image.fromarray(img_array)
+        # We resize the image
+        w, h = img.size
+        new_w = 500
+        new_h = int(h * (new_w/w))
+        img = img.resize((new_w, new_h), Image.LANCZOS)
+        # We convert it to an ImageTk and change the panel image
+        img = ImageTk.PhotoImage(img)
+        panel.configure(image=img)
+        panel.image = img
+        # We repeat
+        panel.after(30, recording_loop)
+        """About transcribing the video"""
+        use_mnist(img_array)
 
-# We create a global variable containing an array representing the picture we are using
+def switch_recording():
+    global capture, out
+    if capture:
+        # We stop the recording
+        record_button.config(text='Start recording')
+        capture.release()
+        out.release()
+        capture = None
+    else:
+        # We start the recording
+        record_button.config(text='Stop recording')
+        capture = cv2.VideoCapture(0)
+        frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter('output.mp4', fourcc, 10.0, (frame_width, frame_height))
+        # We start the loop
+        recording_loop()
+
+# Global variables for the image that is on the screen, the camera and the video being recorded
 global_img = None
+capture = None
+out = None
 
 # We import the MNIST model
 model = keras.models.load_model('model.keras')
@@ -174,16 +151,10 @@ base_options = mp.tasks.BaseOptions(model_asset_path='hand_landmarker.task')
 options = mp.tasks.vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
 detector = mp.tasks.vision.HandLandmarker.create_from_options(options)
 
+
 # We create the window
 panel = Label(root); panel.pack()
-open_img_btn = Button(root, text='Open image', command=open_img).pack()
-screenshot_btn = Button(root, text='Screenshot from camera', command=take_screenshot).pack()
-mnist_model = Button(root, text='Use MNIST model', command=use_mnist).pack()
-
-
-
-
-
+record_button = Button(root, text='Start recording', command=switch_recording); record_button.pack()
 root.mainloop()
 
 
